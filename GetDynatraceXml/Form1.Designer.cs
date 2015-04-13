05b1a9add75cdcec19d7bea72e5d720872ab7d97
Josh Hoffman <jsh139@gmail.com>
2015-04-13 14:06:01 -0400

    GetDynatraceXml

---
+++ GetDynatraceXml/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GetDynatraceXml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDoIt = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnDoIt
+            // 
+            this.btnDoIt.Location = new System.Drawing.Point(316, 52);
+            this.btnDoIt.Name = "btnDoIt";
+            this.btnDoIt.Size = new System.Drawing.Size(135, 48);
+            this.btnDoIt.TabIndex = 0;
+            this.btnDoIt.Text = "Do it";
+            this.btnDoIt.UseVisualStyleBackColor = true;
+            this.btnDoIt.Click += new System.EventHandler(this.btnDoIt_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtDisplay.Location = new System.Drawing.Point(0, 185);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(782, 370);
+            this.txtDisplay.TabIndex = 1;
+            this.txtDisplay.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 555);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btnDoIt);
+            this.Name = "Form1";
+            this.Text = "Get Dynatrace Xml";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDoIt;
+        private System.Windows.Forms.RichTextBox txtDisplay;
+    }
+}
+
---
+++ GetDynatraceXml/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace GetDynatraceXml
+{
+    public partial class Form1 : Form
+    {
+        private const string DashboardLocation = "http://10.140.28.61:8020/rest/management/dashboard/WebRequestsByPurePath";
+        private const string UserName = "josh.hoffman";
+        private const string Password = "trenT000";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoIt_Click(object sender, EventArgs e)
+        {
+            var xml = new XmlDocument();
+
+            try
+            {
+                var request = WebRequest.Create(DashboardLocation);
+                var credential = new NetworkCredential(UserName, Password);
+
+                request.Credentials = credential;
+                request.PreAuthenticate = true;
+
+                var response = request.GetResponse();
+                var answer = response.GetResponseStream();
+
+                if (answer != null)
+                {
+                    using (var stream = new StreamReader(answer))
+                    {
+                        var content = stream.ReadToEnd(); //the string of the whole xml
+                        response.Close();
+                        xml.LoadXml(content);
+                    }
+
+                    txtDisplay.Text = xml.OuterXml;
+                }
+                else
+                {
+                    MessageBox.Show("No data found", MessageBoxIcon.Warning.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, MessageBoxIcon.Error.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
