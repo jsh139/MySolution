55a47c00975b4725404d8ef490ca399f3ead9945
Josh Hoffman <josh.hoffman@sungardas.com>
2017-10-24 10:33:01 -0400

    Update Office interop references

--- DoSomething/Form1.cs
+++ DoSomething/Form1.cs
@@ -52,47 +52,53 @@ namespace DoSomething
 
         private void DoIt(object sender, EventArgs e)
         {
+            var v1 = new Version("1.1.2.1");
+            var v2 = new Version("1.1.2");
+
+            var comp = v1.CompareTo(v2);
+
+
             // var certificate = new X509Certificate2("C:\\Users\\jack.stillwell\\Downloads\\PushSharpCert.p12");
-            var certificate =
-                new X509Certificate2(File.ReadAllBytes("C:\\Users\\josh.hoffman\\Downloads\\PushSharpCert.p12"));
-            var appleConfig =
-                new ApnsConfiguration(ApnsConfiguration.ApnsServerEnvironment.Sandbox, certificate, false)
-                {
-                    InternalBatchSize = 2000,
-                    InternalBatchingWaitPeriod = TimeSpan.FromMilliseconds(500)
-                };
-            var connManager = new ApnsServiceBroker(appleConfig);
-            var notification = new ApnsNotification
-            {
-                DeviceToken = "CDB66775904DFFE078E7FDB76FBBD893C5B6E2A1FFAD0A240955E0A10EEF60E8",
-                Payload = new JObject(
-                    new JProperty("aps",
-                        new JObject(
-                            new JProperty("alert",
-                                new JObject(
-                                    new JProperty("title", "test apns message from assurance title"),
-                                    new JProperty("body", "test apns message from assurance body")
-                                )
-                            )
-                        )
-                    )
-                )
-            };
-            connManager.OnNotificationSucceeded += s =>
-            {
-                txtOutput.AppendText(string.Format("success for message: {0}", s.Payload["aps"]["alert"]));
-                txtOutput.AppendText("hit enter to exit");
-                //Console.ReadLine();
-            };
-            connManager.OnNotificationFailed += (s, ex) =>
-            {
-                txtOutput.AppendText(string.Format("failure for message: {0}", s.Payload["aps"]["alert"]));
-                txtOutput.AppendText("hit enter to exit");
-                //Console.ReadLine();
-            };
-            connManager.Start();
-            connManager.QueueNotification(notification);
-            txtOutput.AppendText("hit enter to exit");
+            //var certificate =
+            //    new X509Certificate2(File.ReadAllBytes("C:\\Users\\josh.hoffman\\Downloads\\PushSharpCert.p12"));
+            //var appleConfig =
+            //    new ApnsConfiguration(ApnsConfiguration.ApnsServerEnvironment.Sandbox, certificate, false)
+            //    {
+            //        InternalBatchSize = 2000,
+            //        InternalBatchingWaitPeriod = TimeSpan.FromMilliseconds(500)
+            //    };
+            //var connManager = new ApnsServiceBroker(appleConfig);
+            //var notification = new ApnsNotification
+            //{
+            //    DeviceToken = "CDB66775904DFFE078E7FDB76FBBD893C5B6E2A1FFAD0A240955E0A10EEF60E8",
+            //    Payload = new JObject(
+            //        new JProperty("aps",
+            //            new JObject(
+            //                new JProperty("alert",
+            //                    new JObject(
+            //                        new JProperty("title", "test apns message from assurance title"),
+            //                        new JProperty("body", "test apns message from assurance body")
+            //                    )
+            //                )
+            //            )
+            //        )
+            //    )
+            //};
+            //connManager.OnNotificationSucceeded += s =>
+            //{
+            //    txtOutput.AppendText(string.Format("success for message: {0}", s.Payload["aps"]["alert"]));
+            //    txtOutput.AppendText("hit enter to exit");
+            //    //Console.ReadLine();
+            //};
+            //connManager.OnNotificationFailed += (s, ex) =>
+            //{
+            //    txtOutput.AppendText(string.Format("failure for message: {0}", s.Payload["aps"]["alert"]));
+            //    txtOutput.AppendText("hit enter to exit");
+            //    //Console.ReadLine();
+            //};
+            //connManager.Start();
+            //connManager.QueueNotification(notification);
+            //txtOutput.AppendText("hit enter to exit");
             //Console.ReadLine();
         }
     }
