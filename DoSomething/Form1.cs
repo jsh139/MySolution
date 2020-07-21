b3406a60161824d0d666ce9528896a1f88be6509
Josh Hoffman <josh.hoffman@assurancesoftware.com>
2020-07-21 16:50:07 -0400

    Add a bunch of Assessments via Scalar.Put

--- DoSomething/Form1.cs
+++ DoSomething/Form1.cs
@@ -8,11 +8,22 @@ using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
 using PushSharp.Apple;
+using SunGardAS.IocContainer;
+using SunGardAS.CMS.Domain.DynamicData.Scalar;
+using SunGardAS.Data.Static;
+using System.Collections.Generic;
+using SunGardAS.CMS.Domain.DataStore.Authentication;
+using SunGardAS.Security.Principal;
+using System.Threading;
+using SunGardAS.Data.Static.Core;
+using SunGardAS;
 
 namespace DoSomething
 {
     public partial class Form1 : Form
     {
+        private const long AdminSecUserId = 411377;
+
         public Form1()
         {
             InitializeComponent();
@@ -52,11 +63,34 @@ namespace DoSomething
 
         private void DoIt(object sender, EventArgs e)
         {
-            var v1 = new Version("1.1.2.1");
-            var v2 = new Version("1.1.2");
+            //var v1 = new Version("1.1.2.1");
+            //var v2 = new Version("1.1.2");
 
-            var comp = v1.CompareTo(v2);
+            //var comp = v1.CompareTo(v2);
 
+            AutoIocConfig.Register();
+
+            RecreatePrincipal();
+
+            var scalarAgent = AutoIoc.Resolve<IScalarAgent>();
+            var assessmentDef = new AssessmentDef();
+
+            for (int i=1; i < 60; i++)
+            {
+                var request = new ScalarPutRequest
+                {
+                    Id = 0,
+                    Entity = AssessmentDef.TablePhysName,
+                    ColumnUpdates = new Dictionary<string, object>
+                    {
+                        { assessmentDef.FkAssessmentTemplate.PhysFullName, 1 },
+                        { assessmentDef.AssessmentId.PhysFullName, $"ASR00{i}" },
+                        { assessmentDef.Name.PhysFullName, i.ToString() },
+                    },
+                };
+
+                scalarAgent.Put(request);
+            }
 
             // var certificate = new X509Certificate2("C:\\Users\\jack.stillwell\\Downloads\\PushSharpCert.p12");
             //var certificate =
@@ -100,6 +134,40 @@ namespace DoSomething
             //connManager.QueueNotification(notification);
             //txtOutput.AppendText("hit enter to exit");
             //Console.ReadLine();
+        }
+
+        private void RecreatePrincipal()
+        {
+            CustomerRec customerRec = null;
+            CustomerExtensionRec customerExtensionRec = null;
+
+            AutoIoc.Using<ICustomerDataStore>(dataStore =>
+            {
+                customerRec = dataStore.GetCustomer(10);
+                customerExtensionRec = dataStore.GetCustomerExtension(10);
+            });
+
+            var adminUser = AutoIoc.Get<IUnauthenticatedSecUserDataStore, SecUserRec>(dataStore =>
+                dataStore.GetSecUser(customerRec, AdminSecUserId));
+            var identity = new SungardAsIdentity(adminUser.UserName, AdminSecUserId, adminUser.FirstName,
+                adminUser.LastName, adminUser.Email, "Admin");
+
+            var customer = new SungardAsCustomer(10, "ldrps")
+            {
+                DbServer = ".\\SQL2017",
+                DbDatabase = "ldrps11",
+                DbUserName = "sa",
+                DbPassword = "Ldrps10@".Secure(),
+                IntegratedLogin = false,
+                UrlId = customerRec.UrlId,
+                MaxNamedUsers = customerRec.MaxNamedUsers,
+            };
+
+            customer.LastMetadataUpdateTime = customerExtensionRec?.LastMetadataUpdateTime;
+            customer.SystemLockoutProcess = customerExtensionRec?.SystemLockoutProcess;
+            customer.SystemLockoutInitiator = customerExtensionRec?.SystemLockoutInitiator;
+
+            Thread.CurrentPrincipal = new SungardAsPrincipal(identity, customer, -2, null);
         }
     }
 }
