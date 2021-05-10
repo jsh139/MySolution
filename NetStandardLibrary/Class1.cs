7b359707e52ed452b62388051df6f5bc707c1918
Josh Hoffman <josh.hoffman@assurancesoftware.com>
2021-05-10 14:21:42 -0400

    New projects TestCoreApp and Net5Library

---
+++ NetStandardLibrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿using opentoken;
+
+namespace Net5Library
+{
+    public class Class1
+    {
+        public string GetUsernameFromSsoOpenToken(string agentConfigFilePath, string openToken)
+        {
+            var agent = new Agent(agentConfigFilePath);
+            var attributes = agent.ReadTokenMultiStringDictionary(token: openToken);
+            if (attributes != null)
+            {
+                return attributes["subject"][0];
+            }
+
+            return null;
+        }
+    }
+}
---
+++ TestCoreApp/Controllers/TwilioTestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestCoreApp.Controllers
+{
+    [ApiController]
+    public class TwilioTestController : ControllerBase
+    {
+        [HttpPost("/TwilioTest")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public IActionResult TwilioTest()
+        {
+            try
+            {
+                //var deliveryId = HttpContext.Request.Form["CallSid"];
+                //var status = HttpContext.Request.Form["CallStatus"];
+
+                return Content("<Response><Say>Hello</Say></Response>", "application/xml");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
